4af427f19c2bb79ba13d9b9226b9b9d18d8e0b66
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2025-07-19 14:25:29 -0300

    Department findById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		Department department =departmentDao.findById(1);
+		
+		System.out.println(department);
 	}
 
 }
