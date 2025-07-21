703448716ab343b2a0504cd13a5b376ad0d14790
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2025-07-21 19:56:18 -0300

    Delete Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -43,6 +43,14 @@ public class Program2 {
 		departmentDao.update(department);
 		System.out.println("Update completed");
 		
+		System.out.println("\"=== TEST 4: Department delete =====\"");
+		System.out.println("Enter id for delete test");
+		int id =sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		
+		
 	}
 
 }
